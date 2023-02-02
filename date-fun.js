5793d9c5dea6b0ca0fa0cab75a541b5c86eb4a0e
Diya Bulsara <diya.bulsara@1Rintern.local>
2023-02-02 18:14:34 +0530

    Javascript methods

---
+++ date-fun.js
@@ -0,0 +1,94 @@
+// date object
+
+var date= new Date();
+console.log(date);
+
+const pastDate= new Date('12-15-2022' )
+document.write(pastDate+"<br>");
+
+let a = date.getDay();
+// document.write(a);
+
+let ar1=['Sunday','Monday',
+'Tuesday',
+'Wednesday',
+'Thursday',
+'Friday',
+'Saturday',
+];
+document.write(ar1[a]+"<br>");
+// let newstr = str.split(" ");
+// document.write(newstr[0],"<br>");
+let h= date.getHours();
+document.write(h+"<br>");
+let m=date.getMinutes();
+let s=date.getSeconds();
+if(h>12)
+{
+    h=h-12;
+    document.write(h +" " +"PM : "+m+" : "+s)
+}
+else{
+    document.write(h +"AM : "+m+" : "+s)
+}
+//square root of given number
+let b;
+function square(b)
+{
+    return Math.sqrt(b);
+}
+document.write(square(4)+"<br>");
+let firstNum;
+let secondNum;
+function maximum(firstNum,secondNum)
+{
+    return Math.max(firstNum,secondNum)
+}
+document.write(maximum(4,5)+"<br>");
+// Random Number Generator
+let z=10;
+document.write(Math.random()*z+"<br>");
+ 
+//  given number is greater or not
+let number=700;
+if(number>100 && number<500)
+{
+    document.write("number is between 100 and 500"+"<br>");
+}
+else{
+    document.write("Number is not between 100 and 500"+"<br>");
+}
+// given numbers are equal or not 
+function equal(x,y)
+{
+    if(x===y)
+    {
+        document.write("true");
+    }
+    else
+    {
+        document.write("false");
+    }
+
+}
+// return true
+equal(1,1);
+equal(1,"1");
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
